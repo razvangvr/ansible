2a93eedc2ede0d8eec634d94737c4b5ff504e389
razvan.gaston <razvan1984@yahoo.com>
2021-04-01 09:32:58 +0300

    [ansible | first steps | github.com/pbassiner/dev-env] copied palybook/steps from, trying to install apt packages

---
+++ .dev-env/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install ansible
+
+ansible-playbook ubuntu.yml -i hosts -vv
